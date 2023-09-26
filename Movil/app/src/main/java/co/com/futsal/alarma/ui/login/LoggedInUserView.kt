9c8acdeee9bcb560d433514c1b717f3bca64fa22
o.tobar@uniandes.edu.co-at-816738610492 <o.tobar@uniandes.edu.co>
2023-09-26 09:01:36 -0500

    se adiciona proyecto movil

---
+++ Movil/app/src/main/java/co/com/futsal/alarma/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package co.com.futsal.alarma.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+    val displayName: String
+    //... other data fields that may be accessible to the UI
+)

d03302d6fbacceabb384a271502fd14d9dff6668
o.tobar@uniandes.edu.co-at-816738610492 <o.tobar@uniandes.edu.co>
2023-09-26 23:51:07 -0500

    se CREA LA VENTANA DE LOGIN

--- Movil/app/src/main/java/co/com/futsal/alarma/ui/home/HomeFragment.kt
+++ Movil/app/src/main/java/co/com/futsal/alarma/ui/home/HomeFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import co.com.futsal.alarma.databinding.FragmentHomeBinding
 
+
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
@@ -27,11 +28,13 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+/*
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        */
+
         return root
     }
 
